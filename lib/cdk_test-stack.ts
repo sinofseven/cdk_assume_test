f03c939f62e9cf5d4d4ba924ab9a032a7002c945
sinofseven <em.s.00001@gmail.com>
2024-04-09 19:19:13 +0900

    fix

--- lib/cdk_test-stack.ts
+++ lib/cdk_test-stack.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import  {Topic} from "aws-cdk-lib/aws-sns";
 
 export class CdkTestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +12,6 @@ export class CdkTestStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkTestQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    new Topic(this, "test", {});
   }
 }
